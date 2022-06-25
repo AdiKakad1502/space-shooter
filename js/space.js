3376f2e6154ac1d16c7ae16a426d1ae661cdc3cc
Aditya Kakad <202052302@iiitvadodara.ac.in>
2022-06-25 10:27:49 +0530

    okk

--- js/space.js
+++ js/space.js
@@ -1,38 +1,47 @@
 var jet = document.getElementById("jet");
+var board = document.getElementById("board");
 
 window.addEventListener("keydown", (e) => {
     var left = parseInt(window.getComputedStyle(jet).getPropertyValue("left"));
-    if ((e.key = "ArrowLeft")) {
-        jet = left - 10 + "px";
-    } else if ((e.key = "ArrowRight")) {
+    if ((e.key == "ArrowLeft" && left > 0)) {
+        jet.style.left = left - 10 + "px";
+    } else if ((e.key == "ArrowRight" && left <= 460)) {
         jet.style.left = left + 10 + "px";
     }
-    if ((e.key == "ArrowUp" || e.keyCode == 32)) {
+    if (e.key == "ArrowUp" || e.keyCode == 32) {
         var bullet = document.createElement("div");
         bullet.classList.add("bullets");
         board.appendChild(bullet);
 
         var movebullet = setInterval(() => {
-
-            var rocks = document.getElementsByClassName("rocks"); 
+            var rocks = document.getElementsByClassName("rocks");
             for (var i = 0; i < rocks.length; i++) {
                 var rock = rocks[i];
-                var rockbound = rock.getBoundingClientRect();
-                var bulletbound = bullet.getBoundingClientRect();
+                if (rock != undefined) {
+                    var rockbound = rock.getBoundingClientRect();
+                    var bulletbound = bullet.getBoundingClientRect();
 
-                if(bulletbound.left >= rockbound.left && bulletbound.right <= rockbound.right && bulletbound.top <= rockbound.top && bulletbound.bottom <= rockbound.bottom ){
-                    rock.parentElement.removeChild(rock);
-                    document.getElementById("points").innerHTML = parseInt(document.getElementById("points").innerHTML) + 1;
+                    if (
+                        bulletbound.left >= rockbound.left &&
+                        bulletbound.right <= rockbound.right &&
+                        bulletbound.top <= rockbound.top &&
+                        bulletbound.bottom <= rockbound.bottom
+                    ) {
+                        rock.parentElement.removeChild(rock);
+                        document.getElementById("points").innerHTML =
+                            parseInt(document.getElementById("points").innerHTML) + 1;
+                    }
                 }
             }
-            var bulletbottom = parseInt(window.getComputedStyle(bullet).getPropertyValue("bottom"));
-            if(bulletbottom >= 500){
+            var bulletbottom = parseInt(
+                window.getComputedStyle(bullet).getPropertyValue("bottom")
+            );
+            if (bulletbottom >= 500) {
                 clearInterval(movebullet);
             }
             bullet.style.left = left + "px";
             bullet.style.bottom = bulletbottom + 3 + "px";
-        
-        },50);
+        });
     }
 });
 
@@ -44,10 +53,10 @@ var generateRocks = setInterval(() => {
     );
     rock.style.left = Math.floor(Math.random() * 450) + "px";
     board.appendChild(rock);
-}, 1500);
+}, 1000);
 
 var moverocks = setInterval(() => {
-    var rocks = document.getElementById("rocks");
+    var rocks = document.getElementsByClassName("rocks");
 
     if (rocks != undefined) {
         for (var i = 0; i < rocks.length; i++) {
@@ -55,13 +64,27 @@ var moverocks = setInterval(() => {
             var rocktop = parseInt(
                 window.getComputedStyle(rock).getPropertyValue("top")
             );
-            if(rocktop >= 475){
+            if (rocktop >= 475) {
                 alert("Game Over");
                 clearInterval(moverocks);
                 window.location.reload();
-
             }
             rock.style.top = rocktop + 25 + "px";
         }
     }
 }, 450);
+
+window.addEventListener("keydown",(e)=>{
+    var key = e.keyCode;
+    if(key === 9){
+        togglePause();
+    }
+});
+function togglePause(){
+    if(!paused){
+        paused = true;
+    }
+    else if(paused){
+        paused = false;
+    }
+}
